1d06c8d002442c8a2e7aafbb7d577f0911b17b2a
Armlockx <reus_julio@hotmail.com>
2024-01-19 17:06:21 -0300

    First commit

---
+++ Entities/Individual.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExxerciseAbstractMethods.Entities
+{
+    class Individual : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+
+        public Individual() { }
+
+        public Individual(string name, double anualIncome ,double healthExpenditures) : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+
+        public override double Tax()
+        {
+            double tax = 0;
+            if (AnualIncome < 20000.00)
+            {
+                tax = AnualIncome * 0.15;
+                if(HealthExpenditures > 0)
+                {
+                    tax = tax - HealthExpenditures * 0.5;
+                }
+            }
+            else if (AnualIncome > 20000.00)
+            {
+                tax = AnualIncome * 0.25;
+                if(HealthExpenditures > 0)
+                {
+                    tax = tax - HealthExpenditures * 0.5;
+                }
+            }
+
+            return tax;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using ExxerciseAbstractMethods.Entities;
+
+namespace ExxerciseAbstractMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string name = "Alex";
+            double anual = 120000.00;
+            double health = 1000.00;
+
+            Individual payer = new Individual(name, anual, health);
+
+            Console.WriteLine(payer.Name + ": $ " + payer.Tax());
+            */
+            /*
+            string name = "MicroBlob";
+            double anual = 400000.00;
+            int employees = 25;
+
+            Company payer = new Company(name, anual, employees);
+
+            Console.WriteLine(payer.Name + ": $ " + payer.Tax().ToString("F2"));
+            */
+            List<TaxPayer> list = new List<TaxPayer>();
+
+            Console.Write("Enter the name of tax payers: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Tax payer #{i} data:");
+                Console.Write("Individual or company (i/c)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Anual income: ");
+                double anual = double.Parse(Console.ReadLine());
+
+                if (ch == 'i')
+                {
+                    Console.Write("Health expenditures: ");
+                    double health = double.Parse(Console.ReadLine());
+                    list.Add(new Individual(name, anual, health));
+                }
+                else if (ch == 'c')
+                {
+                    Console.Write("Number of employees: ");
+                    int employees = int.Parse(Console.ReadLine());
+                    list.Add(new Company(name, anual, employees));
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("bruh");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("TOTAL PAID:");
+            double total = 0;
+            foreach (TaxPayer tp in list)
+            {
+                Console.WriteLine(tp.Name + ": $ " + tp.Tax().ToString("F2"));
+                Console.WriteLine(tp.Name + ": $ " + tp.Tax().ToString("F2"));
+
+            }
+
+            Console.WriteLine();
+            Console.Write("TOTAL TAXES : $" + total.ToString("F2"));
+        }
+    }
+}
